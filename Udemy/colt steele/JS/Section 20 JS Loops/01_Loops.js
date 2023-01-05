e3861bed18ecbe85e545f145e41f8a28f056b3f1
XxFiEnDxX <amitkumargdgps@gmail.com>
2023-01-06 00:22:07 +0530

    pdfs

---
+++ Udemy/colt steele/JS/Section 20 JS Loops/01_Loops.js
@@ -0,0 +1,21 @@
+
+let arr = [3,2,1,6,5,4];
+
+//For Loops
+
+for (let int = 0; int < arr.length; int++) {
+    const ele = arr[int];
+    // console.log(ele);
+}
+
+arr.forEach(ele => {
+    // console.log(ele);
+});
+
+//while Loops
+
+let num = 0;
+while (num < 5) {
+    num++;
+    console.log(num);
+}
